5ceffd5ac71a169c6a3f152d2b56fdb6d337c2ea
Sony Mathew <mailsonymathew@gmail.com>
2021-08-14 21:54:47 +0530

    revised

--- src/main/java/com/mailsonymathew/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/mailsonymathew/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name="currency-exchange")
 
 //CHANGE-KUBERNETES
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
-//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+// Note : The problem with using the K8S service host as env variable is that it depends on the Currency-Exchange service beign up. It is much better to use a customized environment variable as below which we will configure in the deployment.yaml file
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
